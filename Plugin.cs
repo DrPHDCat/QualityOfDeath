3686c3d2f7fecfcc862e2dbf18fcb858a2c0bd8a
DrPHDCat <107643253+DrPHDCat@users.noreply.github.com>
2022-12-23 09:17:50 +0600

    Added source code

---
+++ Plugin.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+
+namespace Quality_Of_Death
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public int RidingTicks = 0;
+        private void Awake()
+        {
+            // Plugin startup logic
+
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        }
+        private void Update()
+        {
+            if (!OptionsManager.Instance.paused)
+            { 
+                if (NewMovement.Instance.ridingRocket)
+                {
+                    if (NewMovement.Instance.hp > 25)
+                    {
+                        RidingTicks = RidingTicks + 1;
+                        if (RidingTicks >= 4)
+                        {
+                            NewMovement.Instance.hp = NewMovement.Instance.hp - 1;
+                            RidingTicks = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ obj/Debug/netstandard2.0/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace Quality_Of_Death
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "Quality_Of_Death";
+        public const string PLUGIN_NAME = "Quality_Of_Death";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
