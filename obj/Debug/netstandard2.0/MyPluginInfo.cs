a678f025f2288608206cf20e19b0ac146ed94cfe
DrPHDCat <107643253+DrPHDCat@users.noreply.github.com>
2022-12-27 10:43:02 +0600

    Add files via upload

---
+++ obj/Debug/netstandard2.0/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace QualityOfDeath
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "QualityOfDeath";
+        public const string PLUGIN_NAME = "QualityOfDeath";
+        public const string PLUGIN_VERSION = "0.0.5";
+    }
+}
