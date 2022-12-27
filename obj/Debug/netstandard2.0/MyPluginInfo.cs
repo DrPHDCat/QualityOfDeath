5a0370dbd8f32976b6062ab4d9fb4fdc6134b837
DrPHDCat <107643253+DrPHDCat@users.noreply.github.com>
2022-12-27 10:41:23 +0600

    Delete obj directory

--- obj/Debug/netstandard2.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace QualityOfDeath
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "QualityOfDeath";
-        public const string PLUGIN_NAME = "QualityOfDeath";
-        public const string PLUGIN_VERSION = "0.0.5";
-    }
-}
